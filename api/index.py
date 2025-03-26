fc4de2cfc166b2b75c021dbe8bd998478940767f
Prem Chotepanit <prem.ch@ku.th>
2025-03-26 12:47:44 +0700

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,8 +1,14 @@
 from flask import Flask
+from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
 
+cors = CORS(app) # allow CORS for all domains on all routes.
+app.config['CORS_HEADERS'] = 'Content-Type'
+
+
 @app.route('/')
+@cross_origin()
 def home():
     return 'Hello, World!'
 
