d5616bdea39ea90e16ff3875ee22aea1cfca78ef
prem <prem.ch@ku.th>
2025-03-26 13:23:05 +0700

    fix:

--- api/index.py
+++ api/index.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 from flask_cors import CORS, cross_origin
 import os
 import requests
@@ -54,5 +54,5 @@ if __name__ == "__main__":
     app.run(
         debug=True,
         host="0.0.0.0",
-        port=5000,
+        port=5001,
     )
