11a783d1c61999e58fec14b4908195904c2ee82c
Prem Chotepanit <prem.ch@ku.th>
2025-03-26 13:01:25 +0700

    Update index.py

--- api/index.py
+++ api/index.py
@@ -18,3 +18,40 @@ def home():
 @app.route('/about')
 def about():
     return 'About'
+
+
+@app.route("/llm/v1/chat/completions", methods=["get", "post"])
+def chat_completion():
+    data = request.json
+    if not data:
+        return jsonify({"error": "Invalid request body"}), 400
+
+    headers = {
+        "Content-Type": "application/json",
+        "Authorization": f"Bearer {LLM_API_KEY}",
+    }
+    json_data = {
+        "model": "claude-3-7-sonnet-20250219",
+        "messages": [
+            {
+                "role": "user",
+                "content": "Hi,
+            },
+        ],
+    }
+
+    response = requests.post(
+        "https://api.anthropic.com/v1/chat/completions",
+        headers=headers,
+        json=json_data,
+    )
+    response.raise_for_status()
+    result = response.json()
+    return result
+
+if __name__ == "__main__":
+    app.run(
+        debug=True,
+        host="0.0.0.0",
+        port=5000,
+    )
